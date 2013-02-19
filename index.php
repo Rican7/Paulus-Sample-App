fe070c1f98a94f4bb9fa2949c145d24150e819df
Trevor Suarez <rican7@gmail.com>
2013-02-19 04:50:03 -0500

    Changing over to the composer auto-loader. Also, setting a quick constant for the framework.

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
 <?php
 
-require_once( __DIR__ . '/vendor/paulus/paulus/autoload.php' );
+require_once( __DIR__ . '/vendor/autoload.php' );
 
 define( 'PAULUS_APP_DIR',  __DIR__ . DIRECTORY_SEPARATOR . 'application' . DIRECTORY_SEPARATOR );
+define( 'PAULUS_EXTERNAL_LIB_DIR', __DIR__ . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR );
 
 use \Paulus\Paulus,
 	\Paulus\Router;
